7bb0d79ebabbff39398b67c2ec470638d03d501f
Nilay.Kothekar <Nilay.Kothekar@westernunion.com>
2024-10-23 15:35:01 +0530

    function written

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>Food Ordering App</h1>
+    </Fragment>
+  );
+};
+
+export default App;
